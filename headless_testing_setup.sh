fa01fef2d08e09700c9759a2e0fdce4b8d35bf1f
Oz Heller <oz.heller@breaktech.com>
2015-06-03 10:48:14 -0500

    Update config.yml for general use, add setup script.

---
+++ headless_testing_setup.sh
@@ -0,0 +1,16 @@
+﻿#!/bin/bash
+#Script to setup testing
+
+cd /
+apt-get update
+apt-get install firefox xvfb -y
+apt-get purge firefox -y
+apt-get install openjdk-7-jre-headless -y
+cp /var/www/vm/firefox-33.0.tar.bz2 /opt
+cd /opt
+tar -xf firefox-33.0.tar.bz2
+mv firefox firefox33
+ln -s /opt/firefox33/firefox /usr/bin/firefox
+mkdir /var/selenium
+cp /var/www/vm/selenium-2.44.0.jar /var/selenium
+cp .bash_aliases ~/
