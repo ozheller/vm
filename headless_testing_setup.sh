0f63d0c65d9e8b9322f95a6009b77df59b4b9469
Oz Heller <oz.heller@breaktech.com>
2015-07-15 17:28:45 -0500

    Removing the firefox and selenium file and add wget lines into the script to download.

--- headless_testing_setup.sh
+++ headless_testing_setup.sh
@@ -6,11 +6,13 @@ apt-get update
 apt-get install firefox xvfb -y
 apt-get purge firefox -y
 apt-get install openjdk-7-jre-headless -y
-cp /var/www/vm/firefox-33.0.tar.bz2 /opt
+wget http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/33.0/linux-x86_64/en-US/firefox-33.0.tar.bz2
+mv firefox-33.0.tar.bz2 /opt
 cd /opt
 tar -xf firefox-33.0.tar.bz2
 mv firefox firefox33
 ln -s /opt/firefox33/firefox /usr/bin/firefox
 mkdir /var/selenium
-cp /var/www/vm/selenium-2.44.0.jar /var/selenium
-cp /var/www/vm/.bash_aliases ~
+wget https://selenium-release.storage.googleapis.com/2.44/selenium-server-standalone-2.44.0.jar
+mv selenium-server-standalone-2.44.0.jar /var/selenium/selenium-2.44.0.jar
+cp /var/www/.bash_aliases ~
